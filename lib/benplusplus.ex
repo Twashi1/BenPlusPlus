aa20ee4e671919d39c13a38887b314637f6576ba
wmhho <who6571@gmail.com>
2024-08-04 11:52:13 +0100

    Basic Expression Calculator

--- lib/benplusplus.ex
+++ lib/benplusplus.ex
@@ -1,10 +1,41 @@
 defmodule Benplusplus do
-  def hello do
-    IO.puts("Hello, World!")
-    "Hi"
+  def expression(str) do
+    str
+    |> String.graphemes()
+    |> token()
+    |> evaluate()
   end
 
-  def expression(str) do
+  defp token(chars, acc \\ [], num \\ "") do
+    case chars do
+      [] ->
+        [acc | [String.to_integer(num)]]
 
+      [char | rest] ->
+        cond do
+          char in ["+", "-", "*", "/"] ->
+            token(rest, [acc| [String.to_integer(num), char]], "")
+          true ->
+            token(rest, acc, num <> char)
+        end
+    end
+  end
+
+  defp evaluate([num | rest]) do
+    evaluate(rest, num)
+  end
+
+  defp evaluate([], result) do
+    result
+  end
+
+  defp evaluate([operator, num | rest], result) do
+    ans = case operator do
+      "+" -> result + num
+      "-" -> result - num
+      "/" -> div(result, num)
+      "*" -> result * num
+    end
+    evaluate(rest, ans)
   end
 end
