86b271b270150220fa135c3312e466fa2ac36d33
Twashi1 <thomas.zarri@gmail.com>
2024-08-04 23:57:40 +0100

    Beginning precedence ordering for parser

--- lib/benplusplus/parser.ex
+++ lib/benplusplus/parser.ex
@@ -1,6 +1,40 @@
 defmodule Benplusplus.Parser do
+  import Benplusplus.Lexer
+
+  @precedence [
+    :value,
+    :add_sub,
+    :multiply_divide,
+    :expression
+  ]
+
+  defp higher_precedence(precedence_level) do
+    case precedence_level do
+      :value -> error("No higher precedence level than value")
+      :add_sub -> :value
+      :multiply_divide -> :add_sub
+      :expression -> :multiply_divide
+    end
+  end
+
   def parse(token_stream) do
-    # Expect declaration
+    # Expect expression
+  end
+
+  defp expression(token_stream, precedence_level) do
+    case precedence_level do
+      # Base case recursion
+      :value ->
+        expect(token_stream, :number)
+      :add_sub ->
+        [lhs | token_stream] = expression(token_stream, higher_precedence(precedence_level))
+        # Check op character
+        [op | _] = token_stream
+
+        if elem(op, 0) in [:plus, :minus] do
+
+        end
+    end
   end
 
   defp error(message) do
