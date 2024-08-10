a1eb1218fd64fc9fa29f3ad7153977d5fb42cec8
wmhho <who6571@gmail.com>
2024-08-10 17:21:23 +0100

    feat: code generator adding to assembly

--- lib/benplusplus/code_generator.ex
+++ lib/benplusplus/code_generator.ex
@@ -4,6 +4,29 @@ defmodule Benplusplus.Codegenerator do
     elem(visit(ast_node), 0)
   end
 
+  def generate_code(ast_node) do
+    code = generate_code(ast)
+  end
+
+  defp generate_code({:number, number}) do
+    ["li t0, #(number)"]
+  end
+
+  defp generate_code({:binop, left, right, op_atom}) do
+    left_code = generate_code(left)
+    right_code = generate_code(right)
+
+    operation_code = case op_atom do
+      '+' -> "add t0, t0, t1"
+      '-' -> "sub t0, t1, t0"
+      '*' -> "mul t0, t0, t1"
+      '/' -> "div t0, t1, t0"
+      _ -> raise("Expected mathematical operator +-*/, got #{op}")
+    end
+
+    left_code ++ ["mv t1, t0"] ++ right_code ++ [operation_code]
+  end
+
   defp visit(head) do
     case head do
       {:number, number} -> Benplusplus.Node.construct_node(:number, number)
